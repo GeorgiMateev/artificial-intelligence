fa781e9fa9a24f713787084a09641073e691db4c
Georgi Mateev <gmmateev92@abv.bg>
2014-10-22 07:03:25 +0300

    added first and second exercise

---
+++ Frogs/Frogs/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Frogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N - the number of frogs watching in one direction.");
+            var n = int.Parse(Console.ReadLine());
+
+            var l = 2*n +1;
+            var state = new int[l];
+            for (int i = 0; i < n; i++)
+            {
+                state[i] = 1;
+                state[l-i-1] = 2;
+            }
+
+            var p = new Program();
+            p.Frogs(state);
+        }
+
+        private void Frogs(int[] state)
+        {
+            this.firstState = this.CopyState(state);
+            this.path = new Stack<int[]>();
+
+            int emptyIndex = 0;
+            for (int i = 0; i < state.Length; i++)
+            {
+                if (state[i] == 0)
+                {
+                    emptyIndex = i;
+                }
+            }
+
+            this.Dfs(state, emptyIndex);
+        }        
+
+        private bool Dfs(int[] state, int emptyIndex)
+        {
+            this.path.Push(this.CopyState(state));
+
+            if (this.isFinal(state))
+            {
+                this.printPath();
+                return true;
+            }
+
+            var moves = new int[] { -1, -2, 1, 2 };
+            foreach (var index in moves)
+	        {
+                int currentIndex = emptyIndex - index;
+		        if (currentIndex  >= 0  && currentIndex < state.Length)
+                {
+                    if ((index < 0 && state[currentIndex] != 2) ||
+                        (index > 0 && state[currentIndex] != 1))
+                    {
+                        continue;
+                    }
+
+                    state[emptyIndex] = state[currentIndex];
+                    state[currentIndex] = 0;
+
+                    var found = this.Dfs(state, currentIndex);
+                    if (found) return true;
+
+                    state[currentIndex] = state[emptyIndex];
+                    state[emptyIndex] = 0;
+
+                    this.path.Pop();
+                }
+	        }
+
+            return false;
+        }
+
+        private void printPath()
+        {
+            while (this.path.Count > 0)
+            {
+                var state = this.path.Pop();
+                foreach (var item in state)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private bool isFinal(int[] state)
+        {
+            bool isFinal = true;
+            for (int i = 0; i < state.Length; i++)
+            {
+                switch (this.firstState[i])
+                {
+                    case 1:
+                        if (state[i] != 2) isFinal = false;
+                        break;
+                    case 2:
+                        if(state[i] != 1) isFinal = false;
+                        break;
+                    case 0:
+                        if (state[i] != 0) isFinal = false;
+                        break;
+                    default:
+                        break;
+                }
+                if (!isFinal) return false;
+            }
+
+            return isFinal;
+        }
+
+        private int[] CopyState(int[] state)
+        {
+            var copy = new int[state.Length];
+            for (int i = 0; i < state.Length; i++)
+            {
+                copy[i] = state[i];
+            }
+
+            return copy;
+        }
+
+        private int[] firstState;
+        private Stack<int[]> path;
+    }
+}
---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void AStar(int[,] startState)
+        {
+            var q = new SortedList<int, int[,]>();
+            var visited = new HashSet<int[,]>();
+
+            var startIndex = Euristics(startState);
+            q.Add(startIndex, startState);
+
+            visited.Add(startState);
+
+            KeyValuePair<int, int[,]> current;
+            while (q.Count > 0)
+            {
+                current = Pop(q);
+
+                if (current.Key == 0)
+                {
+                    break;
+                }
+
+                var neighbours = Neighbours(current);
+
+                foreach (var n in neighbours)
+                {
+                    if (visited.Contains(n.Value))
+                    {
+                        continue;
+                    }
+
+                    //TODO: every node should remember the lenght of the path to itself.
+                    q.Add(n.Key /* + path*/, n.Value);
+                }
+            }
+        }
+
+        private static IList<KeyValuePair<int, int[,]>> Neighbours(KeyValuePair<int, int[,]> current)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static KeyValuePair<int,int[,]> Pop(SortedList<int, int[,]> q)
+        {
+            var element = q.Last();
+            q.RemoveAt(q.Count - 1);
+            return element;
+        }
+
+        private static int Euristics(int[,] state)
+        {
+            //It's a square.
+            var length = state.GetLength(0);
+
+            int pathToGo = 0;
+
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < length; x++)
+                {
+                    var currentNumber = state[y, x];
+                    Point coordinates = GetFinalCoordinatesOfNumber(currentNumber, length);
+
+                    //Add the length of the Manhatan path.
+                    pathToGo += Math.Abs(coordinates.X - x) + Math.Abs(coordinates.Y - y);
+                }
+            }
+
+            return pathToGo;
+        }
+
+        private static Point GetFinalCoordinatesOfNumber(int currentNumber, int dimensionLength)
+        {
+            Point c;
+            if (coordinates.TryGetValue(currentNumber, out c))
+            {
+                return c;
+            }
+
+            if (currentNumber == 0)
+            {
+                c = new Point(dimensionLength - 1, dimensionLength - 1);
+                coordinates[currentNumber] = c;
+                return c;
+            }
+
+            var y = currentNumber / dimensionLength - 1;
+            var x = currentNumber % dimensionLength - 1;
+
+            c = new Point(x, y);
+            coordinates[currentNumber] = c;
+            return c;
+        }
+        
+        private static IDictionary<int, Point> coordinates = new Dictionary<int, Point>();
+
+        struct Point 
+        {
+            public Point(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        struct Node
+        {
+            public int Path { get; set; }
+            public int Euristic { get; set; }
+            public int[,] State { get; set; }
+        }
+    }
+
+    
+}
