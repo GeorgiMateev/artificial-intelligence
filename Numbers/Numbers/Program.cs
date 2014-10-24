d0d2c9756597a1e3e5bcf1551603997ba24bc446
Georgi Mateev <gmmateev92@abv.bg>
2014-10-24 08:27:50 +0300

    Fixes in Numbers

--- Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +10,32 @@ namespace Numbers
     {
         static void Main(string[] args)
         {
+            var input = new int[,]
+            {
+                {6, 5, 3},
+                {2, 4, 8},
+                {7, 0, 1}
+            };
+
+            var finalNode = AStar(input);
+            Console.WriteLine("The path is: ", finalNode.Path);
+
+            var l = finalNode.State.GetLength(0);
+            var state = finalNode.State;
+            for (int y = 0; y < l; y++)
+            {
+                for (int x = 0; x < l; x++)
+                {
+                    Console.Write(state[y, x] + " ");
+                }
+                Console.WriteLine();
+            }
         }
 
-        static void AStar(int[,] startState)
+        static Node AStar(int[,] startState)
         {
-            var q = new SortedList<int, Node>();
-            var visited = new HashSet<Node>();
+            var q = new SortedList<int, Node>(new DuplicateKeyComparer<int>());
+            var visited = new HashSet<int[,]>(new StateEqualityComparer<int[,]>());
 
             var startHeuristics = Heuristics(startState);
 
@@ -29,16 +50,18 @@ namespace Numbers
             while (q.Count > 0)
             {
                 current = Pop(q);
+                visited.Add(current.Value.State);
 
                 if (current.Value.Heuristics == 0)
                 {
-                    break;
+                    return current.Value;
                 }
 
                 var neighbours = Neighbours(current.Value);
 
-                foreach (var neighbour in neighbours)
+                for (var i = 0; i < neighbours.Count; i++)
                 {
+                    var neighbour = neighbours[i];
                     if (visited.Contains(neighbour.State))
                     {
                         continue;
@@ -47,19 +70,65 @@ namespace Numbers
                     neighbour.Path = current.Value.Path + 1;
                     neighbour.Heuristics = Heuristics(neighbour.State);
 
-                    q.Add(neighbour.Path + neighbour.Heuristics, neighbour);
+                    q.Add(neighbour.Path + neighbour.Heuristics, neighbour);                   
                 }
             }
+
+            throw new ApplicationException("Didn't found a solution.");
         }
 
         private static IList<Node> Neighbours(Node node)
         {
+            var neighbours = new List<Node>();
+            var l = node.State.GetLength(0);
+            var zero = node.ZeroIndex;
+
+            var directions = new Point[]
+            {
+                new Point(zero.X + 1, zero.Y),
+                new Point(zero.X, zero.Y + 1),
+                new Point(zero.X - 1, zero.Y),
+                new Point(zero.X, zero.Y -1)
+            };
+
+            foreach (var direction in directions)
+	        {
+		                   
+                if (direction.X < l && direction.X >= 0
+                    && direction.Y < l && direction.Y >= 0)
+                {
+                    var state = CopyState(node.State);
+                    state[zero.Y, zero.X] = state[direction.Y, direction.X];
+                    state[direction.Y, direction.X] = 0;
+                    neighbours.Add(
+                        new Node(state,
+                            new Point(direction.X, direction.Y)));
+                }
+	        }
+
+            return neighbours; 
         }
 
-        private static KeyValuePair<int,int[,]> Pop(SortedList<int, int[,]> q)
+        private static int[,] CopyState(int[,] state)
         {
-            var element = q.Last();
-            q.RemoveAt(q.Count - 1);
+            var l = state.GetLength(0);
+            var copy = new int[l, l];
+
+            for (int y = 0; y < l; y++)
+            {
+                for (int x = 0; x < l; x++)
+                {
+                    copy[y, x] = state[y, x];
+                }
+            }
+
+            return copy;
+        }
+
+        private static KeyValuePair<int, Node> Pop(SortedList<int, Node> q)
+        {
+            var element = q.First();
+            q.RemoveAt(0);
             return element;
         }
 
@@ -110,7 +179,7 @@ namespace Numbers
 
         private static Point FindZeroCoordinates(int[,] state)
         {
-            var l = state.Length;
+            var l = state.GetLength(0);
 
             for (int y = 0; y < l; y++)
             {
@@ -122,6 +191,8 @@ namespace Numbers
                     }
                 }
             }
+
+            return new Point();
         }
         
         private static IDictionary<int, Point> coordinates = new Dictionary<int, Point>();
@@ -129,6 +200,7 @@ namespace Numbers
         struct Point 
         {
             public Point(int x, int y)
+                : this()
             {
                 this.X = x;
                 this.Y = y;
@@ -143,9 +215,10 @@ namespace Numbers
             public int Heuristics { get; set; }
             public int[,] State { get; set; }
 
-            public int ZeroIndex { get; set; }
+            public Point ZeroIndex { get; set; }
 
             public Node(int path, int heuristics, int[,] state)
+                : this()
             {
                 this.Path = path;
                 this.Heuristics = heuristics;
@@ -153,11 +226,73 @@ namespace Numbers
             }
 
             public Node(int[,] state)
+                : this()
             {
                 this.State = state;
             }
-        }
-    }
 
-    
+            public Node(int[,] state, Point zeroIndex)
+                : this()
+            {
+                this.State = state;
+                this.ZeroIndex = zeroIndex;
+            }
+        }
+
+        public class DuplicateKeyComparer<TKey>
+                :IComparer<TKey> where TKey : IComparable
+        {
+            #region IComparer<TKey> Members
+
+            public int Compare(TKey x, TKey y)
+            {
+                int result = x.CompareTo(y);
+
+                if (result == 0)
+                    return 1;   // Handle equality as beeing greater
+                else
+                    return result;
+            }
+
+            #endregion
+        }
+
+        public class StateEqualityComparer<TKey>
+                : IEqualityComparer<TKey>
+        {
+
+            public bool Equals(TKey lft, TKey r)
+            {
+                var left = lft as int[,];
+                var right = r as int[,];
+
+                var l = left.GetLength(0);
+
+                for (int y = 0; y < l; y++)
+                {
+                    for (int x = 0; x < l; x++)
+                    {
+                        if (left[y, x] != right[y, x]) return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public int GetHashCode(TKey obj)
+            {
+                var state = obj as int[,];
+                var l = state.GetLength(0);
+                var builder = new StringBuilder();
+                for (int y = 0; y < l; y++)
+                {
+                    for (int x = 0; x < l; x++)
+                    {
+                        builder.Append(state[y, x]);
+                    }
+                }
+                return builder.ToString().GetHashCode();
+            }
+        }
+    }    
 }
