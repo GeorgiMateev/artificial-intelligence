4abf5afead0b4a0277a9fba91224b8601836af7c
Georgi Mateev <gmmateev92@abv.bg>
2014-11-09 16:17:46 +0200

    Completed the genetic algorithm

--- Genetic-tsm/Genetic-tsm/Program.cs
+++ Genetic-tsm/Genetic-tsm/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
+using System.Diagnostics;
 
 namespace Genetic_tsm
 {
@@ -10,10 +11,17 @@ namespace Genetic_tsm
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Number of cities:");
             var n = int.Parse(Console.ReadLine());
             var points = GeneratePoints(n);
 
-            Genetic(points, 100, 100);
+            Console.WriteLine("Population size:");
+            var population = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Epochs:");
+            var epochs = int.Parse(Console.ReadLine());
+
+            Genetic(points, population, epochs);
         }        
 
         private static void Genetic(Point[] points, int populationSize, int epochs)
@@ -27,7 +35,19 @@ namespace Genetic_tsm
 
                 population.AddRange(mutated);
 
-                Evolution(population, populationSize);
+                population = Evolution(population, populationSize);
+
+                // Print the length of the shortest path after the 10th, 1/4, 2/4, 3/4 and the last epoch.
+                if (i == 9 ||
+                    i == Math.Floor(epochs * 1 / 4d) ||
+                    i == Math.Floor(epochs * 1 / 2d) ||
+                    i == Math.Floor(epochs * 3 / 4d) ||
+                    i == epochs -1)
+                {
+                    var shortestPath = population[0];
+                    var length = Fitness(shortestPath);
+                    Console.WriteLine("On {0} epoch the shortest path length is: {1}", i + 1, length);
+                }
             }
         }
 
@@ -36,17 +56,19 @@ namespace Genetic_tsm
         /// </summary>
         /// <param name="population"></param>
         /// <param name="populationSize"></param>
-        private static void Evolution(IList<Point[]> population, int populationSize)
+        private static List<Point[]> Evolution(IList<Point[]> population, int populationSize)
         {
-            var sortedPopulation = new SortedDictionary<int, Point[]>();
+            var sortedPopulation = new SortedDictionary<double, Point[]>(new DuplicateKeyComparer<double>());
             for (int i = 0; i < population.Count; i++)
             {
                 sortedPopulation.Add(
                     Fitness(population[i]),
-                    population);
+                    population[i]);
             }
 
-            var survived = population.Reverse().Take(populationSize);
+            var survived = sortedPopulation.Values
+                .Take(populationSize)
+                .ToList();
             return survived;
         }
 
@@ -55,9 +77,25 @@ namespace Genetic_tsm
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
-        private static int Fitness(Point[] path)
+        private static double Fitness(Point[] path)
         {
-            throw new NotImplementedException();
+            var length = 0d;
+            for (int i = 0; i < path.Length - 1; i++)
+            {
+                length += Distance(path[i], path[i + 1]);
+            }
+
+            length += Distance(path[path.Length - 1], path[0]);
+
+            return length;
+        }
+
+        private static double Distance(Point point1, Point point2)
+        {
+            var xDelta = point1.X - point2.X;
+            var yDelta = point1.Y - point2.Y;
+
+            return Math.Sqrt(Math.Pow(xDelta, 2) + Math.Pow(yDelta, 2));
         }
 
         /// <summary>
@@ -67,13 +105,15 @@ namespace Genetic_tsm
         /// <returns></returns>
         private static IList<Point[]> CrossOver(IList<Point[]> population)
         {
-            var cross = new List<Point>();
+            var cross = new List<Point[]>();
             var r = new Random();
             for (int i = 0; i < population.Count; i++)
             {
                 var index = r.Next(population.Count);
                 cross.Add(population[index]);
             }
+
+            return cross;
         }
 
         private static IList<Point[]> Mutate(IList<Point[]> crossed)
@@ -85,10 +125,10 @@ namespace Genetic_tsm
                 var mPath = crossed[i];
                 var fPath = crossed[i + 1];
                 
-                var pivot = r.Next(mPath.Count);
+                var pivot = r.Next(mPath.Length);
 
-                var sorted = new SortedDictionary<int, Point>;
-                for (int e = pivot; e < mPath.Count; e++)
+                var sorted = new SortedDictionary<int, Point>();
+                for (int e = pivot; e < mPath.Length; e++)
                 {
                     var element = mPath[e];
                     var elementIndex = FindIndex(fPath, element);
@@ -103,7 +143,7 @@ namespace Genetic_tsm
 
                 mutatedPath.AddRange(sorted.Values.ToList());
 
-                mutated.Add(mutatedPath);
+                mutated.Add(mutatedPath.ToArray());
             }
 
             return mutated;
@@ -113,7 +153,7 @@ namespace Genetic_tsm
         {
             for (int i = 0; i < population.Length; i++)
             {
-                if (population[i].Id = element.Id)
+                if (population[i].Id == element.Id)
                 {
                     return i;
                 }
@@ -124,14 +164,33 @@ namespace Genetic_tsm
 
         private static List<Point[]> GeneratePopulation(Point[] points, int populationSize)
         {
-            throw new NotImplementedException();
+            var population = new List<Point[]>();
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                population.Add(RandomArrayTool.RandomizeArray(points));
+            }
+
+            return population;
         }
 
         private static Point[] GeneratePoints(int n)
         {
-            throw new NotImplementedException();
+            var r = new Random();
+            var maxDistance = 100;
+            var points = new Point[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var x = r.Next(maxDistance);
+                var y = r.Next(maxDistance);
+                points[i] = new Point(x, y, r.Next(10000));
+            }
+
+            return points;
         }
 
+        [DebuggerDisplay("Id: {Id}, X: {X}, Y: {Y}")]
         public struct Point
         {
             public Point (int x, int y, int id) 
@@ -145,6 +204,55 @@ namespace Genetic_tsm
             public int X { get; set; }
             public int Y { get; set; }
             public int Id { get; set; }
+        }
+
+        public class DuplicateKeyComparer<TKey>
+                : IComparer<TKey> where TKey : IComparable
+        {
+            #region IComparer<TKey> Members
+
+            public int Compare(TKey x, TKey y)
+            {
+                int result = x.CompareTo(y);
+
+                if (result == 0)
+                    return 1;   // Handle equality as beeing greater
+                else
+                    return result;
+            }
+
+            #endregion
+        }
+
+        static class RandomArrayTool
+        {
+            static Random _random = new Random();
+
+            public static Point[] RandomizeArray(Point[] arr)
+            {
+                var list = new List<KeyValuePair<int, Point>>();
+                // Add all strings from array
+                // Add new random int each time
+                foreach (var s in arr)
+                {
+                    list.Add(new KeyValuePair<int, Point>(_random.Next(), s));
+                }
+                // Sort the list by the random number
+                var sorted = from item in list
+                             orderby item.Key
+                             select item;
+                // Allocate new string array
+                var result = new Point[arr.Length];
+                // Copy values to array
+                int index = 0;
+                foreach (KeyValuePair<int, Point> pair in sorted)
+                {
+                    result[index] = pair.Value;
+                    index++;
+                }
+                // Return copied array
+                return result;
+            }
         }
     }
 }
