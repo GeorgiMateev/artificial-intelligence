9f1de3e9e9457fbf32f0ac70e96ec638bc2c0a42
Georgi Mateev <gmmateev92@abv.bg>
2014-12-10 08:47:47 +0200

    Implemented backpropagation of the error

--- NeuralNetwork/NeuralNetwork/Program.cs
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -31,8 +31,9 @@ namespace NeuralNetwork
 
             var hiddenLayerNeurons = 10;
             var iterations = 5;
+            var learningConst = 0.1;
             var net = new NeuralNetwork(4, 3, hiddenLayerNeurons);
-            net.Train(data, iterations);
+            net.Train(data, iterations, learningConst);
         }
     }
 
@@ -50,22 +51,36 @@ namespace NeuralNetwork
         #endregion
 
         #region Public methods
-        public void Train(IEnumerable<double> dataEntry)
+        public void Train(IEnumerable<double> dataEntry, double learningConst)
         {
             var attributes = dataEntry.Take(dataEntry.Count() - 1);
             var attrVector = Vector<double>.Build.DenseOfEnumerable(attributes);
             this.ActivateLayers(attrVector);
+
+            var expectedClass = (int)dataEntry.Last();
+            var expectedOutput = this.ExpectedOutput(expectedClass, this.activations[2].Count);
+
+            var outputErrors = this.OutputErrors(expectedOutput);
+            var hiddenLayerErrors = this.HiddenLayerErrors(this.activations[1], outputErrors);
+
+            this.UpdateWeights(this.weights[1], activations[1], outputErrors, learningConst);
+            this.UpdateWeights(this.weights[0], activations[0], hiddenLayerErrors, learningConst);
         }
 
-        public void Train(IList<IEnumerable<double>> data, int iterations)
+        public void Train(IList<IEnumerable<double>> data, int iterations, double learningConst)
         {
             for (int i = 0; i < iterations; i++)
             {
                 foreach (var entry in data)
                 {
-                    this.Train(entry);
+                    this.Train(entry, learningConst);
                 }
             }
+        }
+
+        public Vector<double> GetOutput()
+        {
+            return this.activations.Last();
         }
         #endregion
 
@@ -73,8 +88,7 @@ namespace NeuralNetwork
         private void InitWeights(int firstLayerNeurons, int secondLayerNeurons, int index)
         {
             // Create matrix filled with random numbers from -0.005 to 0.005
-            var weights = Matrix<double>.Build.Random(
-                                                      secondLayerNeurons,
+            var weights = Matrix<double>.Build.Random(secondLayerNeurons,
                                                       firstLayerNeurons,
                                                       new ContinuousUniform(-0.05, 0.05, new MersenneTwister(133)));
 
@@ -95,6 +109,38 @@ namespace NeuralNetwork
         private double Logistic(double sum)
         {
             return 1 / (1 + Math.Pow(Math.E, (-sum))); 
+        }
+
+        private Vector<double> ExpectedOutput(int expectedClass, int outputNeurons)
+        {
+            var output = Vector<double>.Build.Sparse(outputNeurons);
+            output[expectedClass - 1] = 1.0;
+            return output;
+        }
+
+        private Vector<double> OutputErrors(Vector<double> expected)
+        {
+            var output = this.GetOutput();
+            return output.MapIndexed((i, x) => x * (1 - x) * (expected[i] - x));
+        }
+
+        private Vector<double> HiddenLayerErrors(Vector<double> activations, Vector<double> outputErrors)
+        {
+            var errors = this.weights[1].Transpose() * outputErrors.ToColumnMatrix();
+            var vector = errors.EnumerateColumns().First();
+            vector.MapIndexedInplace((i, x) => x * (1 - x) * activations[i]);
+            return vector;
+        }
+
+        private void UpdateWeights(Matrix<double> weights, Vector<double> activations, Vector<double> errors, double learningConst)
+        {
+            var rows = weights.EnumerateRowsIndexed();
+
+            foreach (var row in rows)
+            {
+                var error = errors[row.Item1];
+                row.Item2.MapIndexedInplace((i, x) => x + learningConst * error * activations[i]);
+            }
         }
         #endregion
 
