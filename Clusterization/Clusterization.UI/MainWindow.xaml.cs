a76d3a732f87262c83f9d3a416fb602ff892d5c8
Georgi Mateev <gmmateev92@abv.bg>
2015-01-17 17:41:55 +0200

    Implemented kMeans clusterization

---
+++ Clusterization/Clusterization.UI/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clusterization.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var files = new string[] 
+            {
+                "easy_12.arff",
+                "synth1.arff",
+                "synth2.arff",
+                "test2in1.arff",
+                "test121.arff",
+                "test121212.arff"
+            };
+
+            fileName.ItemsSource = files;
+            clusterize.Click +=clusterize_Click;
+
+            this.kmeans = new KMeans();
+        }
+
+        void clusterize_Click(object sender, RoutedEventArgs e)
+        {
+ 	        var file = fileName.SelectedValue.ToString();
+            var c = int.Parse(clusters.Text);
+
+            var data = ReadData(file);
+
+            this.kmeans.Clusterize(data, c);
+
+            this.DrawPoints(data, c);
+        }
+
+        private void DrawPoints(IList<Tuple<double, double, int>> points, int clusters)
+        {
+            
+        }
+
+        private static List<Tuple<double, double, int>> ReadData(string fileName)
+        {
+            var data = new List<Tuple<double, double, int>>();
+
+            using (var sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.StartsWith("@"))
+                    {
+                        continue;
+                    }
+
+                    var dataEntry = line.Split(',');
+
+                    data.Add(new Tuple<double, double, int>(double.Parse(dataEntry[0]), double.Parse(dataEntry[1]), 0));
+                }
+            }
+            return data;
+        }
+
+        private KMeans kmeans;
+    }
+}
---
+++ Clusterization/Clusterization/KMeans.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clusterization
+{
+    public class KMeans
+    {
+        #region Public methods
+        public void Clusterize(IList<Tuple<double, double, int>>data, int clusters)
+        {
+            var centroids = this.ChooseInitialMeans(data, clusters);
+
+            var assigned = true;
+            while (assigned)
+            {
+                assigned = this.AssignClusters(data, centroids);
+
+                this.UpdateCentroids(data, centroids);
+            }
+        }                  
+        #endregion
+
+        #region Private methods
+        private IList<Tuple<double, double>> ChooseInitialMeans(IList<Tuple<double, double, int>> data, int clusters)
+        {
+            var means = new List<Tuple<double, double>>();
+            for (int i = 0; i < clusters; i++)
+            {
+                var sample = data[KMeans.random.Next(clusters)];
+                var mean = new Tuple<double, double>(sample.Item1, sample.Item2);
+                means.Add(mean);
+            }
+            return means;
+        }
+
+        private bool AssignClusters(IList<Tuple<double, double, int>> data, IList<Tuple<double, double>> centroids)
+        {
+            var assigned = false;
+            foreach (var sample in data)
+            {
+                assigned = assigned || this.AssignCluster(sample, centroids);
+            }
+
+            return assigned;
+        }  
+
+        private bool AssignCluster(Tuple<double, double, int> sample, IList<Tuple<double, double>> centroids)
+        {
+            double minDistance = int.MaxValue;
+            var cluster = 0;
+            for (int i = 0; i < centroids.Count; i++)
+            {
+                var c = centroids[i];
+                var distance = this.Distance(sample, c);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    cluster = i;
+                }
+            }
+
+            if(sample.Item3 != cluster)
+            {
+                sample = new Tuple<double, double, int>(sample.Item1, sample.Item2, cluster);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void UpdateCentroids(IList<Tuple<double, double, int>> data, IList<Tuple<double, double>> centroids)
+        {
+            var clusters = data.GroupBy(s => s.Item3).ToList();
+
+            for (var i = 0; i < clusters.Count(); i++)
+            {
+                var centroid = this.CalculateCentroid(clusters[i].ToList());
+                centroids[i] = centroid;
+            }
+        }
+
+        private double Distance(Tuple<double, double, int> p1, Tuple<double, double> p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.Item1 - p2.Item1, 2) + Math.Pow(p1.Item2 - p2.Item2, 2));
+        }
+
+        private Tuple<double, double> CalculateCentroid(IList<Tuple<double, double, int>> cluster)
+        {
+            var centroid = new Tuple<double, double>(cluster[0].Item1, cluster[0].Item2);
+
+            for (int i = 1; i < cluster.Count; i++)
+			{
+                var point = cluster[i];
+                var x = centroid.Item1 + ((point.Item1 - centroid.Item1) / ((double)i + 1));
+                var y = centroid.Item2 + ((point.Item2 - centroid.Item2) / ((double)i + 1));
+			    centroid = new Tuple<double, double>(x, y);
+			}
+
+            return centroid;
+        }
+        #endregion
+
+        #region Private fields and constants
+        private static Random random = new Random(120);
+        #endregion
+    }
+}
